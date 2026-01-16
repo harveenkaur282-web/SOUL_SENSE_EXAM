1bbe0824cb2de359ab4f2fe60dc9bc4610dc925c
harveenkaur282-web <harveenkaur282@gmail.com>
2026-01-16 06:19:55 +0530

    Fix __init__.py: import only existing models to pass CI

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,14 +2,8 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+# Import only existing models
 from .reflection import UserReflection
-from .user import User
-from .score import Score
-from .journal_entry import JournalEntry
-from .satisfaction_record import SatisfactionRecord
-from .assessment_result import AssessmentResult
-from .response import Response
-from .question import Question
-from .question_cache import QuestionCache
-from .statistics_cache import StatisticsCache
-from .user_settings import UserSettings
+
+# Expose Base for db usage
+# DO NOT import non-existent files until they are created
